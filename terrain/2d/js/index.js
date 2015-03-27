9625f960bb7c10770036ca0c838a98ea4e7502c1
Christopher LaBauve <jiminychris@gmail.com>
2015-03-27 16:21:46 -0500

    Cleaning things up and removing mouse controls.

--- terrain/2d/js/index.js
+++ terrain/2d/js/index.js
@@ -1,6 +1,7 @@
 var THREE = require('three');
 var $ = require('jquery');
 var FlyControls = require('./FlyControls');
+var Noise = require('./Noise');
 
 function main()
 {
@@ -8,79 +9,23 @@ function main()
     var NUM_MODES = 2;
     var clock = new THREE.Clock();
     var timer = new THREE.Clock(false);
-    // set up the plane vars
-    var planeWidth = 10;
-    var planeHeight = planeWidth;
-    var segments = planeWidth*5;
-
-    var VIEW_ANGLE = 45,
-        ASPECT = window.innerWidth / window.innerHeight,
-        NEAR = 0.1,
-        FAR = 100000000000;
 
     var scene = new THREE.Scene();
-    var camera =
-      new THREE.PerspectiveCamera(
-        VIEW_ANGLE,
-        ASPECT,
-        NEAR,
-        FAR);
-    var controls = new FlyControls(camera);
 
-    controls.movementSpeed = 1;
-    controls.domElement = container;
-    controls.rollSpeed = Math.PI / 6;
-    controls.autoForward = false;
-    controls.dragToLook = true;
+    var plane = initPlane(scene);
+    var light = initLight(scene);
+    var controls = initCamera(scene);
+    var camera = controls.object;
 
-    var renderer = new THREE.WebGLRenderer({
-        //antialiasing: true
-    });
+
+    var renderer = new THREE.WebGLRenderer();
     //renderer.shadowMapEnabled = true;
     renderer.setSize(window.innerWidth, window.innerHeight);
+
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.PlaneGeometry(planeWidth, planeHeight, segments, segments);
-    var material = new THREE.MeshPhongMaterial({
-        color: 0xEDD46D,
-        shininess: .0001,
-        emissive: 0x111001,
-        metal: false,
-        shading: THREE.SmoothShading
-        /*wireframe: true*/
-    });
-
-    var sunGeometry = new THREE.SphereGeometry(695800000);
-    var sunMaterial = new THREE.MeshBasicMaterial({
-        color: 0xFFFFFF,
-        /*wireframe: true*/
-    });
-
-    var plane = new THREE.Mesh(geometry, material);
-    //plane.castShadow = true;
-    //plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2.0;
-
-    var lightPos = new THREE.Vector3(15, 4, -15).normalize().multiplyScalar(149600000000);
-    var light = new THREE.PointLight(0xFFFFFF, 1);
-    light.position.copy(lightPos);
-
-    var sun = new THREE.Mesh(sunGeometry, sunMaterial);
-    sun.position.copy(lightPos);
-    var sunAxis = new THREE.Vector3(-15, 0, 15);
-
-    var ambient = new THREE.AmbientLight(0x202020);
-
     //var edges = new THREE.VertexNormalsHelper(plane, 2, 0x00ff00, 1);
-
-    scene.add(plane);
-    scene.add(sun);
-    scene.add(light);
-    scene.add(ambient);
     //scene.add(edges);
-
-    camera.position.z = 10;
-    camera.position.y = 1.5;
 
     $(window).keypress(function(e)
     {
@@ -92,21 +37,21 @@ function main()
             if (mode == 0)
             {
                 timer.stop();
-                material.color.setHex(0xEDD46D);
+                plane.material.color.setHex(0xEDD46D);
                 //material.needsUpdate = true;
             }
             else if (mode == 1)
             {
                 timer.start();
-                material.color.setHex(0x19CFE3);
+                plane.material.color.setHex(0x19CFE3);
                 //material.needsUpdate = true;
             }
         }
     });
 
-    var frame = 0;
     function update()
     {
+        var geometry = plane.geometry;
         var delta = clock.getDelta();
         var elapsedTime = timer.getElapsedTime();
         for (var i = 0; i < geometry.vertices.length; ++i)
@@ -131,9 +76,89 @@ function main()
     requestAnimationFrame(update);
 }
 
-$(document).ready(function() {
-    main();
-});
+function initPlane(scene)
+{
+    var planeWidth = 10;
+    var planeHeight = planeWidth;
+    var segments = planeWidth*5;
+
+    var geometry = new THREE.PlaneGeometry(planeWidth, planeHeight, segments, segments);
+    var material = new THREE.MeshPhongMaterial({
+        color: 0xEDD46D,
+        shininess: .0001,
+        shading: THREE.SmoothShading
+        /*wireframe: true*/
+    });
+
+    var plane = new THREE.Mesh(geometry, material);
+    //plane.castShadow = true;
+    //plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2.0;
+
+    scene.add(plane);
+
+    return plane;
+}
+
+function initLight(scene)
+{
+    var sunRadius = 695800000;
+    var sunDistance = 149600000000;
+    var sunColor = 0xFFFFFF;
+    var lightColor = 0xFFFFFF;
+    var ambientColor = 0x202020;
+    var lightIntensity = 1;
+    var lightPos = new THREE.Vector3(15, 4, -15).normalize().multiplyScalar(sunDistance);
+
+    var sunGeometry = new THREE.SphereGeometry(sunRadius);
+    var sunMaterial = new THREE.MeshBasicMaterial({
+        color: sunColor,
+        /*wireframe: true*/
+    });
+    var sun = new THREE.Mesh(sunGeometry, sunMaterial);
+    sun.position.copy(lightPos);
+
+    var sunlight = new THREE.PointLight(lightColor, lightIntensity);
+    sunlight.position.copy(lightPos);
+
+    var ambient = new THREE.AmbientLight(ambientColor);
+
+    scene.add(sun);
+    scene.add(sunlight);
+    scene.add(ambient);
+
+    return {
+        sun: sun,
+        sunlight: sunlight,
+        ambient: ambient
+    };
+}
+
+function initCamera()
+{
+    var VIEW_ANGLE = 45,
+        ASPECT = window.innerWidth / window.innerHeight,
+        NEAR = 0.1,
+        FAR = 100000000000;
+
+    var camera =
+      new THREE.PerspectiveCamera(
+        VIEW_ANGLE,
+        ASPECT,
+        NEAR,
+        FAR);
+    camera.position.z = 10;
+    camera.position.y = 1.5;
+    var controls = new FlyControls(camera);
+
+    controls.movementSpeed = 1;
+    controls.domElement = container;
+    controls.rollSpeed = Math.PI / 6;
+    controls.autoForward = false;
+    controls.dragToLook = true;
+
+    return controls;
+}
 
 function terrainHeight(x, y, t)
 {
@@ -148,63 +173,12 @@ function terrainHeight(x, y, t)
     {
         var amplitude = Math.pow(persistence, i + amplitudeOffset);
         var frequency = Math.pow(persistence, i + frequencyOffset)
-        var noise = noise3d(x / frequency, y / frequency, t / frequency);
+        var noise = Noise.noise3d(x / frequency, y / frequency, t / frequency);
         height += amplitude * noise;
     }
     return Math.pow(3, height);
 }
 
-var permutation = [
-    151,160,137, 91, 90, 15,131, 13,201, 95, 96, 53,194,233,  7,225,140, 36,
-    103, 30, 69,142,  8, 99, 37,240, 21, 10, 23,190,  6,148,247,120,234, 75,
-      0, 26,197, 62, 94,252,219,203,117, 35, 11, 32, 57,177, 33, 88,237,149,
-     56, 87,174, 20,125,136,171,168, 68,175, 74,165, 71,134,139, 48, 27,166,
-     77,146,158,231, 83,111,229,122, 60,211,133,230,220,105, 92, 41, 55, 46,
-    245, 40,244,102,143, 54, 65, 25, 63,161,  1,216, 80, 73,209, 76,132,187,
-    208, 89, 18,169,200,196,135,130,116,188,159, 86,164,100,109,198,173,186,
-      3, 64, 52,217,226,250,124,123,  5,202, 38,147,118,126,255, 82, 85,212,
-    207,206, 59,227, 47, 16, 58, 17,182,189, 28, 42,223,183,170,213,119,248,
-    152,  2, 44,154,163, 70,221,153,101,155,167, 43,172,  9,129, 22, 39,253,
-     19, 98,108,110, 79,113,224,232,178,185,112,104,218,246, 97,228,251, 34,
-    242,193,238,210,144, 12,191,179,162,241, 81, 51,145,235,249, 14,239,107,
-     49,192,214, 31,181,199,106,157,184, 84,204,176,115,121, 50, 45,127,  4,
-    150,254,138,236,205, 93,222,114, 67, 29, 24, 72,243,141,128,195, 78, 66,
-    215, 61,156,180
-];
-var p = [];
-for (var i=0; i < 256*2 ; i++)
-{
-    p.push(permutation[i % 256]);
-}
-
-function noise3d(x, y, z) {
-    var X = Math.floor(x) & 255,                  // FIND UNIT CUBE THAT
-        Y = Math.floor(y) & 255,                  // CONTAINS POINT.
-        Z = Math.floor(z) & 255;
-    x -= Math.floor(x);                                // FIND RELATIVE X,Y,Z
-    y -= Math.floor(y);                                // OF POINT IN CUBE.
-    z -= Math.floor(z);
-    var u = fade(x),                                // COMPUTE FADE CURVES
-        v = fade(y),                                // FOR EACH OF X,Y,Z.
-        w = fade(z);
-    var A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z,      // HASH COORDINATES OF
-        B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;      // THE 8 CUBE CORNERS,
-
-    return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),  // AND ADD
-                                 grad(p[BA  ], x-1, y  , z   )), // BLENDED
-                         lerp(u, grad(p[AB  ], x  , y-1, z   ),  // RESULTS
-                                 grad(p[BB  ], x-1, y-1, z   ))),// FROM  8
-                 lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),  // CORNERS
-                                 grad(p[BA+1], x-1, y  , z-1 )), // OF CUBE
-                         lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
-                                 grad(p[BB+1], x-1, y-1, z-1 ))));
-}
-function fade(t) { return t * t * t * (t * (t * 6 - 15) + 10); }
-function lerp(t, a, b) { return a + t * (b - a); }
-function grad(hash, x, y, z) {
-    var h = hash & 15;                      // CONVERT LO 4 BITS OF HASH CODE
-    var u = h<8 ? x : y,                 // INTO 12 GRADIENT DIRECTIONS.
-        v = h<4 ? y : h==12||h==14 ? x : z;
-    return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
-}
-
+$(document).ready(function() {
+    main();
+});
